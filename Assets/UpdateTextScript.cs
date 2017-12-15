30444995daa9aee0b694826ee3798df37f014e57
Jack Hu <xhu@ball.com>
2017-12-15 17:50:18 -0500

    added more missing files

---
+++ Assets/UpdateTextScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateTextScript : MonoBehaviour {
+
+    Text txtBox;
+    public GameObject NMEAManager;
+    private NMEANetworkReader reader;
+    
+
+	// Use this for initialization
+	void Start () {
+        txtBox = gameObject.GetComponent<Text>();
+        reader = NMEAManager.GetComponent<NMEANetworkReader>();
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reader.getLatestPayload() != null)
+        {
+            txtBox.text = reader.getLatestPayload().ToString();
+        }
+    }
+}
